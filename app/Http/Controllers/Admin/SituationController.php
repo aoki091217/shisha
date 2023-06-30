ed1cb33be3afdcfbb0a5869991fd7e41611f1ecf
aoki091217 <aojun091217@gmail.com>
2023-06-30 22:42:26 +0900

    fix

--- app/Http/Controllers/Admin/SituationController.php
+++ app/Http/Controllers/Admin/SituationController.php
@@ -84,6 +84,8 @@ class SituationController extends Controller
                             $newCarousel->fill($insert)->save();
 
                             foreach ($carousel['actions'] as $action) {
+                                if (is_null($action['action'])) continue;
+
                                 $carouselAction = new CarouselAction();
                                 $carouselAction->fill(array_merge($action, ['carousel_id' => $newCarousel->id]))->save();
                             }
@@ -181,6 +183,8 @@ class SituationController extends Controller
                             $newCarousel->fill($insert)->save();
 
                             foreach ($carousel['actions'] as $action) {
+                                if (is_null($action['action'])) continue;
+
                                 $carouselAction = new CarouselAction();
                                 $carouselAction->fill(array_merge($action, ['carousel_id' => $newCarousel->id]))->save();
                             }
