6501fde5b3723e81c313c2e61a86a6188bb2cb89
aoki091217 <aojun091217@gmail.com>
2023-11-30 02:48:41 +0900

    fix

--- resources/js/bill.js
+++ resources/js/bill.js
@@ -54,9 +54,11 @@ $(window).on('load', function () {
     $('#searchButton, #reloadButton').on('click', function () {
         getCustomers().done(function (customerShops) {
             $('.table-wrapper tbody tr').remove();
+            customerShops.sort(function (leftItem, rightItem) {
+                return (leftItem.visited_at < rightItem.visited_at ? 1 : -1);
+            });
 
             $.each(customerShops, function (index, item) {
-                console.log(item);
                 let selectButton = $('<button>', {
                     type: 'button',
                     class: 'btn btn-sm btn-outline-primary w-100',
