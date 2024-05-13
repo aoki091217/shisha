5f119dd3b7a43aa816e3eba3e78c85704d8c1093
tak-solder <tak.solder@gmail.com>
2024-05-13 23:08:54 +0900

    customer_shopsに追加したカラムを削除

--- app/Models/CustomerShop.php
+++ app/Models/CustomerShop.php
@@ -3,8 +3,6 @@
 namespace App\Models;
 
 use Carbon\Carbon;
-use DB;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -14,30 +12,9 @@ class CustomerShop extends Model
     use HasFactory;
     use SoftDeletes;
 
-    public const FRIEND_STATUS_FOLLOWED = 'followed';
-    public const FRIEND_STATUS_UNFOLLOWED = 'unfollowed';
-    public const FRIEND_STATUS_UNKNOWN = 'unknown';
-    public const FRIEND_STATUSES = [
-        self::FRIEND_STATUS_FOLLOWED,
-        self::FRIEND_STATUS_UNFOLLOWED,
-        self::FRIEND_STATUS_UNKNOWN,
-    ];
-
-    public const LIFF_STATUS_ACTIVE = 'active';
-    public const LIFF_STATUS_INACTIVE = 'inactive';
-    public const LIFF_STATUS_UNKNOWN = 'unknown';
-    public const LIFF_STATUSES = [
-        self::LIFF_STATUS_ACTIVE,
-        self::LIFF_STATUS_INACTIVE,
-        self::LIFF_STATUS_UNKNOWN,
-    ];
-
     protected $fillable = [
         'customer_id',
         'shop_id',
-        'friend_status',
-        'liff_status',
-        'activated_at',
         'visited_at',
     ];
 
@@ -92,29 +69,5 @@ class CustomerShop extends Model
         if (isset($words['visited_minute'])) {
             $query->whereRaw("DATE_FORMAT(visited_at, '%i') = '{$words['visited_minute']}'");
         }
-    }
-
-    protected function friendStatus(): Attribute
-    {
-        return Attribute::make(
-            set: function (string $value) {
-                if (!in_array($value, self::FRIEND_STATUSES)) {
-                    throw new \InvalidArgumentException('Invalid friend_status');
-                }
-                return $value;
-            },
-        );
-    }
-
-    protected function liffStatus(): Attribute
-    {
-        return Attribute::make(
-            set: function (string $value) {
-                if (!in_array($value, self::LIFF_STATUSES)) {
-                    throw new \InvalidArgumentException('Invalid liff_status');
-                }
-                return $value;
-            },
-        );
     }
 }
